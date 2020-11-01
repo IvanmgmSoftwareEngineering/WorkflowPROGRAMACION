d8254e9d4b30dbc67b3bda5db213b44f93a1f24e
IvanMGM <ivanmgm88@gmail.com>
2020-11-01 12:05:43 +0100

    error en nh corregido

---
+++ prueba.h
@@ -0,0 +1,20 @@
+/*
+ * TÍTULO: libreria.h
+ * AUTOR: Iván Martín Gómez
+ * INSTRUCCIONES DE USO: 
+ * --- AQUÍ SÓLO LAS CABECERAS DE LAS FUNCIONES. DEBO CREAR UN FICHERO
+ * --- CON EL MISMO NOMBRE QUE ESTE FICHERO, PERO CON EXTENSIÓN .C,
+ * --- EN EL QUE SE IMPLEMENTARÁN CADA UNA DE LAS FUNCIONES DE LAS
+ * --- CUALES AQUI SE MUESTRAN LAS CABECERAS
+ * 
+ *      tds = Tipo Dato Salida
+ *      tde = Tipo Dato Entrada
+ */
+//----------------------------------------------------------------------------
+
+#ifndef _LIBRERIA_H
+#define _LIBRERIA_H
+	tds nombre_funcion_1 (tde Argumento_1, tde Argumento_2, tde Argumento_n);
+	tds nombre_funcion_2 (tde Argumento_1, tde Argumento_2, tde Argumento_n);
+	tds nombre_funcion_m (tde Argumento_1, tde Argumento_2, tde Argumento_n);
+#endif
